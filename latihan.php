b8184d12e4818ce3efcce8e2fc9e5134725957ae
M. Ardy Ikhsanudin <121593174+ardyikhsanudin@users.noreply.github.com>
2022-12-30 09:55:49 +0700

    Create latihan.php

---
+++ latihan.php
@@ -0,0 +1,7 @@
+    <?php
+    echo "<h2> Data Pribadi </h2>";
+    echo "Nama : M. Ardy Ikhsanudin ", "<br>";
+    echo "Alamat : Jatipuro, Karanganyar ", "<br>";
+    echo "No. Telp : 081238827726";
+    ?>
+
